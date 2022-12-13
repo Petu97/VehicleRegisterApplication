d00ea4bf172b284f1b7e2e154c1c07ab90163e48
Petteri <petu_97@hotmail.com>
2022-12-13 09:37:23 +0200

    Model classes vehicle and truck data annotations

--- Models/Truck.cs
+++ Models/Truck.cs
@@ -1,13 +1,17 @@
 ﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 
 namespace VehicleRegisterApplication.Models
 {
     public class Truck : Vehicle
     {
         [JsonProperty("truckType")]
+        [Required]
         public TruckType truckType;
 
         [JsonProperty("cargoCapacity")]
+        [Required]
+        [Range(0, 500)] //yea i got no idea how big trucks can get
         public float cargoCapacity; 
     }
 }
--- Models/Vehicle.cs
+++ Models/Vehicle.cs
@@ -9,18 +9,24 @@ namespace VehicleRegisterApplication.Models
         public int id { get; set; }
 
         [JsonProperty("brand")]
+        [Required]
+        [StringLength(20)]
         public string vehicleBrand { get; set; }
 
         [JsonProperty("model")]
+        [StringLength(20)]
         public string vehicleModel { get; set; }
 
         [JsonProperty("register")]
+        [StringLength(10)]
         public string lisencePlateNumber { get; set; }
 
         [JsonProperty("vehicleOwner")]
+        [StringLength(30)]
         public string owner { get; set; }
 
         [JsonProperty("wheels")]
+        [Range(2, 16)]
         public int numOfWheels { get; set; }
     }
 }
